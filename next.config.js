3d47cc8ad1c69f2121881776efe67120aa1f963a
sanci mothe <nulstaco@gmail.com>
2025-10-01 12:19:34 +0000

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
