88f1fb9a76ac27d9d66f562b830800233b4c1cec
sanci mothe <nulstaco@gmail.com>
2025-10-04 15:20:21 +0000

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@
 
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDeviceDetect } from "@/providers/DeviceDetectProvider";
 import { getTitle } from "@/utils";
 import { useKeyPressEvent } from "react-use";
 import Board from "../components/Board";
 import useGameModel from "../hooks/useGameModel";
 import LevelSelector from "@/components/LevelSelector";
+import MenuCard from "@/components/MenuCard";
 
 export default function Home() {
   const { isConnected, status } = useAccount();
   const router = useRouter();
+  const [isMenuVisible, setIsMenuVisible] = useState(true);
 
   useEffect(() => {
     if (status === 'disconnected') {
@@ -32,31 +34,39 @@ export default function Home() {
     );
   }
 
+  const handlePlayClick = () => {
+    setIsMenuVisible(false);
+  };
+
   return (
-    <div
-      onClick={isMobile ? action : undefined}
-      className="flex flex-col items-center h-full"
-    >
-      <h1 className="text-3xl md:text-5xl font-semibold mt-5 mb-8 md:mb-20 text-center">
-        {getTitle(state.gameStatus)}
-        <div className="opacity-90 text-base md:text-lg italic font-normal">
-          {isMobile ? "(Touch to play)" : "(Press SPACEBAR to play)"}
+    <>
+      {isMenuVisible && <MenuCard onPlayClick={handlePlayClick} />}
+
+      <div
+        onClick={isMobile ? action : undefined}
+        className="flex flex-col items-center h-full"
+      >
+        <h1 className="text-3xl md:text-5xl font-semibold mt-5 mb-8 md:mb-20 text-center">
+          {getTitle(state.gameStatus)}
+          <div className="opacity-90 text-base md:text-lg italic font-normal">
+            {isMobile ? "(Touch to play)" : "(Press SPACEBAR to play)"}
+          </div>
+        </h1>
+        <div className="flex items-center justify-between w-80 md:w-96 mb-6 md:mb-10">
+          <div className="text-2xl font-medium">
+            Score:
+            <span className="font-semibold text-amber-400 [text-shadow:_0_4px_4px_var(--tw-shadow-color)] shadow-amber-500 ml-1">
+              {gameScore}
+            </span>
+          </div>
+          <LevelSelector
+            level={gameLevel}
+            setLevel={setGameLevel}
+            gameState={state.gameStatus}
+          />
         </div>
-      </h1>
-      <div className="flex items-center justify-between w-80 md:w-96 mb-6 md:mb-10">
-        <div className="text-2xl font-medium">
-          Score:
-          <span className="font-semibold text-amber-400 [text-shadow:_0_4px_4px_var(--tw-shadow-color)] shadow-amber-500 ml-1">
-            {gameScore}
-          </span>
-        </div>
-        <LevelSelector
-          level={gameLevel}
-          setLevel={setGameLevel}
-          gameState={state.gameStatus}
-        />
+        <Board rows={state.rows} />
       </div>
-      <Board rows={state.rows} />
-    </div>
+    </>
   );
 }
